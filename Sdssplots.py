d18b0edfcda9ccfcaf8498d52f5799d99092731e
Dan Filler <danfiller1@gmail.com>
2013-07-10 17:03:14 -0400

    This should be up to current

---
+++ Sdssplots.py
@@ -0,0 +1,26 @@
+##  This is a plotting module for the plotting of data in Scicoder 2013.
+#
+
+import numpy as np
+import matplotlib.pyplot as plt
+#from matplotlib.gridspec import GridSpec
+import matplotib.pyplot as plt
+def easy_hist(data,col_names):
+    """ This is a function that will create plots for various types of data:
+    
+    Parameters
+    ----------
+    data: list of float arrays
+    col_names: string list
+    """
+    length=size(data) #this determines the number of plots to be made
+    plt.subplots_adjust(hspace=.4)
+    plt.subplots_adjust(hspace=.4)
+
+    for i in xrange(length):
+        idx=131+i
+        plt.subplot(idx)            
+        plt.title(col_names[i])
+        plt.hist(data[i])
+#plt.savefig(sometitlethatwillbefilledinlater.png)
+    plt.show()
