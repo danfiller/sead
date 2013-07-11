4fd6193b14c4f510dad03d9a598d913e7093af09
EliseJ <elise.jennings@gmail.com>
2013-07-11 15:26:11 -0500

    typos corrected

--- sdss_hist.py
+++ sdss_hist.py
@@ -62,8 +62,9 @@ if __name__ == "__main__":
     if args.gal_coords == True:
         g_distance, x_above, y_above, height_above_disc = dist_conv.easy_con(rtn_col_list[-3],rtn_col_list[-2],rtn_col_list[-1])
         #Edit the list of arrays passed to plot fn
-        rtn_col_list.remove(-2)  #remove "L" data
-        rtn_col_list.remove(-3)  #remove "B" data
+        rtn_col_list.pop(-2)  #remove "B" data
+        rtn_col_list.pop(-3)  #remove "L" data
+        rtn_col_list.pop(-1)  #remove "DIST_ADOP" data
         rtn_col_list.append(g_dist)  
         #Edit the list of columns
         args.col_list.remove("L")  
